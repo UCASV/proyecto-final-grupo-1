bd067bf8d47127b386d7579099add22a8a64f9b3
Rodrigo José Aguirre Arévalo <62662445+Aguirre221@users.noreply.github.com>
2021-06-18 09:27:39 -0600

    Add files via upload

---
+++ Proyecto_POO/login.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Proyecto_POO
+{
+    partial class login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.img1 = new System.Windows.Forms.PictureBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img1
+            // 
+            this.img1.Image = ((System.Drawing.Image)(resources.GetObject("img1.Image")));
+            this.img1.Location = new System.Drawing.Point(179, 59);
+            this.img1.Name = "img1";
+            this.img1.Size = new System.Drawing.Size(158, 113);
+            this.img1.TabIndex = 0;
+            this.img1.TabStop = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lbl1.Location = new System.Drawing.Point(157, 184);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(197, 25);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "INICIO DE SESION ";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUser.Location = new System.Drawing.Point(154, 239);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(200, 31);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPass.Location = new System.Drawing.Point(154, 292);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(200, 31);
+            this.txtPass.TabIndex = 3;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.img1);
+            this.Name = "login";
+            this.Text = "Log In";
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
+
