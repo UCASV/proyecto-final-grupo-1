61e85a8a6626a17161e62ed27d3163422803d95d
DanielSolis00209020 <00209020@uca.edu.sv>
2021-06-25 00:03:54 -0600

    AdminView2

---
+++ Project_POO/View/Admin/FrmCreateCenter/FrmCreateCenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_POO.View.Admin.FrmCreateCenter
+{
+    public partial class FrmCreateCenter : Form
+    {
+        public FrmCreateCenter()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Clean_Click(object sender, EventArgs e)
+        {
+            txt_AddressCenter.Text = "";
+            txt_EmailCenter.Text = "";
+            txt_TelCenter.Text = "";
+            cmb_EmployeeInChargeCenter.SelectedItem = null;
+            cmb_TypeCenter.SelectedItem = null;
+        }
+    }
+}
---
+++ Project_POO/View/Admin/FrmRegist/FrmRegist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project_POO
+{
+    public partial class FrmRegist : Form
+    {
+        public FrmRegist()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Clean_Click(object sender, EventArgs e)
+        {
+            txt_Address.Text = "";
+            txt_Conf_Pass.Text = "";
+            txt_Email.Text = "";
+            txt_Pass.Text = "";
+            txt_Name.Text = "";
+            cmb_Type.SelectedItem = null;
+        }
+    }
+}
